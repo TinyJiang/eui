501187db0f6cce358be57231d5f0c4007d19c409
蒋骏峰 <jiangjf@eastcom-sw.com>
2015-04-30 14:48:15 +0800

    增加font-awesome依赖，并将加载图标改为spin

--- src/comps/tree/tree.js
+++ src/comps/tree/tree.js
@@ -3,8 +3,8 @@
  * Tree组件
  *
  * @event init()初始化结束
- * @event select(nodeRecord)选中
- * @event unselect(nodeRecord)取消选中
+ * @event select(nodeRecord) 选中
+ * @event unselect(nodeRecord) 取消选中
  * @event nodeclick(nodeRecord)单击node
  * @event expand(nodeRecord)展开node
  * @event collapseNode(nodeRecord)收缩node
@@ -12,24 +12,70 @@
  * @since 0.1
  * @author JJF
  */
-define(['eui/core/clz', 'eui/base/CompBase', 'eui/data/loader'], function(clz, CompBase, loader) {
-    var Tree = clz.define({
-        name: 'Tree',
-        parent: CompBase,
-        preConstructor: function(c) {
-            var d = c.dom;
-            if (!utils.isObjOf(c.loader, loader)) {
-                c.loader = loader.create(c.loader);
-            }
-            return [c]
-        },
-        afterConstructor: function() {
-            var me = this,
-                c = me.getConf();
-            domInit(me);
-            bindEvents(me);
-        },
-        proto: {}
-    });
+define(['eui/core/clz', 'eui/base/CompBase', 'eui/data/loader', 'eui/core/register'],
+    function(clz, CompBase, loader, register) {
+        /** ----------------公共参数、方法-----------------* */
+        // 初始化dom
+        var initDom = function(_tree) {
+            var dom = _tree.getDom();
+            dom.addClass('euitree');
+        }
 
-});
+        // 渲染数据
+        var renderData = function(_tree, records) {
+            var d = _grid.getDom(),
+                conf = _grid.getConf(),
+                columns = conf.columns,
+                renderObj = {
+                    id: conf.id,
+                    type: conf.type,
+                    headers: [],
+                    datas: []
+                };
+
+            d.empty().append($(html));
+        }
+
+        // 绑定事件
+        var bindEvents = function(_tree) {
+            // 逻辑事件
+            var c = _tree.getConf(),
+                loader = c.loader;
+            loader.on('beforeload', function() {
+                _tree.controlLoadMask(true);
+            });
+            loader.on('load', function(data) {
+                renderData(_tree, data);
+            });
+
+            // ui事件
+            var d = _grid.getDom();
+        }
+
+
+
+        /** ----------------类定义-----------------* */
+        var Tree = clz.define({
+            name: 'Tree',
+            parent: CompBase,
+            preConstructor: function(c) {
+                var d = c.dom;
+                if (!utils.isObjOf(c.loader, loader)) {
+                    c.loader = loader.create(c.loader);
+                }
+                return [c]
+            },
+            afterConstructor: function() {
+                var me = this,
+                    c = me.getConf();
+                domInit(me);
+                bindEvents(me);
+            },
+            proto: {}
+        });
+
+        return register(Tree, {
+            tree: 'create'
+        })
+
+    });
